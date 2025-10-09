41600abf49fefc056e6120aee8199e58e62148eb
Dora Do <doradocodes@gmail.com>
2025-10-09 17:42:57 -0400

    Initial code

---
+++ script.js
@@ -0,0 +1,23 @@
+// insert card elements here
+const containerElement = document.querySelector('.container');
+
+class Card {
+    // your code goes here
+    constructor() {
+
+    }
+
+    // add your class functions here
+}
+
+function getData() {
+    // your code goes here
+}
+
+function displayCards(cards) {
+    // your code goes here
+}
+
+// call your functions here
+
+
