048e038282c17d3d0c065c4f3507c0a2c26f7427
Jorge Auquilla <jorge.auquilla99@myhunter.cuny.edu>
2025-10-14 01:05:02 -0400

    update

--- script.js
+++ script.js
@@ -8,8 +8,29 @@ class Card {
         this.name = name;
         this.number = number;
         this.image = image;
+        this.element.classList.add("Card");
     }
     // add your class functions here
+
+    create_return_HTML(){
+            //A method to create and return an HTML element for the card
+            this.element.innerHTML = "";
+
+            const nameElement = document.createElement("p");
+            nameElement.classList.add('card_name');
+            nameElement.innerText = this.name;
+            this.element.appendChild(nameElement);
+
+            const numberElement = document.createElement("p");
+            numberElement.classList.add("card_number");
+            numberElement.innerText = this.number;
+            this.element.appendChild(numberElement);
+
+            const imageElement = document.createElement("img");
+            imageElement.classList.add("card_image");
+            imageElement.innerText = this.image;
+            this.element.appendChild(imageElement);
+    }
 }
 
 function getData() {
@@ -17,15 +38,23 @@ function getData() {
     fileName = "data.json";
     fetch(fileName)
         .then(response => response.json())
-        .then(data => console.log(data));
+        .then(data => displayCards(data));
 }
 
 function displayCards(cards) {
     // your code goes here
+
+    for(let i = 0; i < data.length; i++){
+
+    }
 }
 
 // call your functions here
 getData();
 displayCards();
+
+
+// - When the data is loaded, loop through each object and create a new card instance for each one.
+// - Append each card to the page (e.g. inside a `.deck` container).
 
 
