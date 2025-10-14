a9f08d10d3cd8bc4cb1e3d2c87877876e1b38b91
Jorge Auquilla <jorge.auquilla99@myhunter.cuny.edu>
2025-10-14 00:40:18 -0400

    start

--- script.js
+++ script.js
@@ -3,15 +3,21 @@ const containerElement = document.querySelector('.container');
 
 class Card {
     // your code goes here
-    constructor() {
-
+    constructor(element, name, number, image) {
+        this.element = element;
+        this.name = name;
+        this.number = number;
+        this.image = image;
     }
-
     // add your class functions here
 }
 
 function getData() {
     // your code goes here
+    fileName = "data.json";
+    fetch(fileName)
+        .then(response => response.json())
+        .then(data => console.log(data));
 }
 
 function displayCards(cards) {
@@ -19,5 +25,7 @@ function displayCards(cards) {
 }
 
 // call your functions here
+getData();
+displayCards();
 
 
